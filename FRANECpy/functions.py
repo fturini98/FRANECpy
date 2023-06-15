579e5387f59c95e081076d9db27c8b7883b68ae1
Francesco Turini <fturini.turini7@gmail.com>
2023-06-15 17:03:24 +0200

    inizio creazione diagrammi HR
    -Quello per uguale massa ci entra una volta per uguale massa e diversa metall.
        da correggere
    
    -Da implementare i file RAW.

--- FRANECpy/functions.py
+++ FRANECpy/functions.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import re
+
+def print_tree(tree, indent=''):
+    for key, value in tree.items():
+        if isinstance(value, dict):
+            print(f"{indent}{key}")
+            print_tree(value, indent + '  ')
+        elif isinstance(value,(list)):
+            print(f"{indent}{key}")
 
 def clear_tree(tree, target_name):
     cleared_tree = {}
@@ -6,7 +17,6 @@ def clear_tree(tree, target_name):
     for key, values in tree.items():
         if key == target_name:
             for metall, masses in values.items():
-                print(metall)
                 if metall not in cleared_tree:
                     cleared_tree[metall] = {}
                 metall_folder = cleared_tree[metall]
@@ -18,3 +28,79 @@ def clear_tree(tree, target_name):
                 cleared_tree.update(subtree)
     
     return cleared_tree
+
+def HR_plot_equal_metall(tree,metal,mass_min=None,mass_max=None):
+    if metal in tree:
+        masses=tree[metal]
+    else:
+        print(f"No data for {metal}.Exit!")
+        sys.exit(0)
+    for mass, df in masses.items():
+        match = re.search(r'M([\d.]+)', mass)
+        mass=float(match.group(1))
+        
+        if (mass_min is None or mass_min<= mass) and (mass_max is None or mass<=mass_max):
+            
+            # Plotting logic here
+            plt.plot(df["LOG_TE_(K)"], df["LOG_L/Lo"], label=f"Mass {mass} ({metal})")
+
+    # Customize the plot
+    plt.xlabel("LOG_TE_(K)")
+    plt.ylabel("LOG_L/Lo")
+    plt.title(f"Different mass for {metal}")
+    plt.legend()
+    plt.grid(True)
+
+    # Show the plot
+    plt.show()
+
+def HR_plot_equal_mass(tree,mass,Z_min=None,Z_max=None,He_min=None,He_max=None):
+    
+    if not isinstance(mass,str):
+        mass="{:.2f}".format(mass)
+        mass_value=mass
+        mass="M"+str(mass)
+        
+    for metal in tree.keys():
+        match=re.search(r'Z([\d.]+)_He([\d.]+)',metal)
+        Z=float(match.group(1))
+        He=float(match.group(2))
+        if (Z_min!=None and Z<Z_min) or (Z_max!=None and Z_max<Z) or (He_min!=None and He<He_min) or (He_max!=None and He_max<He):
+            print(f"Helium or metallicity out of range. Excluding Z:{Z} and He:{He}") 
+        else:
+            df=tree[metal][mass]
+            plt.plot(df["LOG_TE_(K)"], df["LOG_L/Lo"], label=f"Mass {mass} ({metal})")
+    
+    # Customize the plot
+    plt.xlabel("LOG_TE_(K)")
+    plt.ylabel("LOG_L/Lo")
+    plt.title(f"Different metalicity for mass={mass_value} M_sun")
+    plt.legend()
+    plt.grid(True)
+
+    # Show the plot
+    plt.show()
+
+def HR_plot_RID(tree,equal="metallicity",mass_min=None,mass_max=None,Z_min=None,Z_max=None,He_min=None,He_max=None):
+    if not all(key.startswith("Z") for key in tree.keys()):
+        tree=clear_tree(tree,"RID")
+        
+    if equal in ["metall","metal","metallicity"]:    
+        for metal in tree.keys():
+            match=re.search(r'Z([\d.]+)_He([\d.]+)',metal)
+            Z=float(match.group(1))
+            He=float(match.group(2))
+                
+            if (Z_min!=None and Z<Z_min) or (Z_max!=None and Z_max<Z) or (He_min!=None and He<He_min) or (He_max!=None and He_max<He):
+                print(f"Helium or metallicity out of range. Excluding Z:{Z} and He:{He}") 
+            else:            
+                HR_plot_equal_metall(tree,metal,mass_min,mass_max)
+                
+    elif equal in ["mass", "Mass"]:
+        for metal,masses in tree.items():
+            for mass in masses:
+                match = re.search(r'M([\d.]+)', mass)
+                mass=float(match.group(1))
+                if (mass_min is None or mass_min<= mass) and (mass_max is None or mass<=mass_max):
+                    print("mass")
+                    HR_plot_equal_mass(tree,mass,Z_min=None,Z_max=None,He_min=None,He_max=None)
